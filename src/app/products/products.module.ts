6f4758e04ca224c0c8eceaf7db3d25c83ec1f271
Jagan Panigrahy <jagannathpanigrahy2012@gmail.com>
2020-04-11 01:11:21 +0530

    Entered toolbar

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductDataService } from './product-data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductsComponent } from './products/products.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductsComponent],
   imports: [
     CommonModule,
-    ProductsRoutingModule
-  ]
+    ProductsRoutingModule,
+    HttpClientModule
+  ],
+  providers : [ProductDataService]
 })
 export class ProductsModule { }
